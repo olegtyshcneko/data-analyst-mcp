c6a5efc9e85360aaffe9f4a1f961522e6ec8848c
Oleg Tyshchenko <oleg.tyshchenko@gmail.com>
2026-05-11 22:55:14 +0200

    red: plot line returns a valid PNG

--- tests/test_plots.py
+++ tests/test_plots.py
@@ -79,3 +79,9 @@ def test_plot_bar_returns_valid_png(call_tool, load_df_into_session):
     load_df_into_session("d", _NUMERIC_DF)
     result = call_tool("plot", {"name": "d", "kind": "bar", "x": "g"})
     _assert_valid_png(result)
+
+
+def test_plot_line_returns_valid_png(call_tool, load_df_into_session):
+    load_df_into_session("d", _NUMERIC_DF)
+    result = call_tool("plot", {"name": "d", "kind": "line", "x": "x", "y": "y"})
+    _assert_valid_png(result)
