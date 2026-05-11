620a94f858fe9109c3d5125b97dc5af00158a4b6
Oleg Tyshchenko <oleg.tyshchenko@gmail.com>
2026-05-11 22:58:17 +0200

    red: plot violin returns a valid PNG

--- tests/test_plots.py
+++ tests/test_plots.py
@@ -99,3 +99,9 @@ def test_plot_box_returns_valid_png(call_tool, load_df_into_session):
     load_df_into_session("d", _NUMERIC_DF)
     result = call_tool("plot", {"name": "d", "kind": "box", "x": "g", "y": "y"})
     _assert_valid_png(result)
+
+
+def test_plot_violin_returns_valid_png(call_tool, load_df_into_session):
+    load_df_into_session("d", _NUMERIC_DF)
+    result = call_tool("plot", {"name": "d", "kind": "violin", "x": "g", "y": "y"})
+    _assert_valid_png(result)
